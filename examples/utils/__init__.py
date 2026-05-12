c7f44765f0746f39427cf07d407be3e81c25b632
Dr. Benkovics János <benkovicsdr@gmail.com>
2026-05-12 23:40:04 +0200

    feat: add examples/utils with audio_io and time_format
    Created examples/utils/ directory with audio I/O and Hungarian time formatting utilities:
    - audio_io.py: record_audio() and AudioPlayer for 24kHz audio
    - time_format.py: magyar_ido_szoveggel() for Hungarian time strings
    
    Restored from master branch with updated imports (removed unused agents import).
    
    ADW-ID: issue-8-alkalmazs-kd-thelyezse-example

---
+++ examples/utils/__init__.py
@@ -0,0 +1,6 @@
+"""Utility functions for examples: audio I/O, time formatting."""
+
+from examples.utils.audio_io import record_audio, AudioPlayer
+from examples.utils.time_format import magyar_ido_szoveggel
+
+__all__ = ["record_audio", "AudioPlayer", "magyar_ido_szoveggel"]
